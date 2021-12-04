4ca471b297b04b6dfddf3e6d6b2e33a7c3adb6d1
ikusteu <ivan.kust57@gmail.com>
2021-12-04 23:10:20 +0100

    Add check if pull request

--- src/createSummary.ts
+++ src/createSummary.ts
@@ -12,54 +12,56 @@ export default async ({
   summaryTitle?: string;
   index: number;
 }) => {
-  const octokit = github.getOctokit(token);
+  //   const octokit = github.getOctokit(token);
 
-  const {
-    repo,
-    payload: {
-      head_commit: { id: sha },
-    },
-  } = github.context;
+  console.log("Event name > ", github.context.eventName);
+  console.log("SHA > ", github.context.payload.pull_request.head.sha);
+  //   const {
+  //     repo,
+  //     payload: {
+  //       head_commit: { id: sha },
+  //     },
+  //   } = github.context;
 
-  // create title for custom check summary
-  const titlePrefix = `${summaryTitle} ${index}:`;
-  const fileTitle = filepath
-    .split("/")
-    .slice(-1)[0]
-    .replace(/[-_]/g, " ")
-    .replace(/\.(md|html|log)/, "");
-  const title = [titlePrefix, `"${fileTitle}"`].join(" ");
+  //   // create title for custom check summary
+  //   const titlePrefix = `${summaryTitle} ${index}:`;
+  //   const fileTitle = filepath
+  //     .split("/")
+  //     .slice(-1)[0]
+  //     .replace(/[-_]/g, " ")
+  //     .replace(/\.(md|html|log)/, "");
+  //   const title = [titlePrefix, `"${fileTitle}"`].join(" ");
 
-  // create empty (in-progress) summary
-  const {
-    data: { id },
-  } = await octokit.rest.checks.create({
-    head_sha: sha,
-    name: title,
-    status: "in_progress",
-    output: {
-      summary: "",
-    },
-    ...repo,
-  });
+  //   // create empty (in-progress) summary
+  //   const {
+  //     data: { id },
+  //   } = await octokit.rest.checks.create({
+  //     head_sha: sha,
+  //     name: title,
+  //     status: "in_progress",
+  //     output: {
+  //       summary: "",
+  //     },
+  //     ...repo,
+  //   });
 
-  // read file to update
-  const summary = await new Promise<string>((res) => {
-    fs.readFile(filepath, (err, data) => {
-      if (err) throw err;
-      res(data.toString());
-    });
-  });
+  //   // read file to update
+  //   const summary = await new Promise<string>((res) => {
+  //     fs.readFile(filepath, (err, data) => {
+  //       if (err) throw err;
+  //       res(data.toString());
+  //     });
+  //   });
 
-  // update check summary
-  await octokit.rest.checks.update({
-    check_run_id: id,
-    status: "completed",
-    conclusion: "success",
-    output: {
-      title,
-      summary,
-    },
-    ...repo,
-  });
+  //   // update check summary
+  //   await octokit.rest.checks.update({
+  //     check_run_id: id,
+  //     status: "completed",
+  //     conclusion: "success",
+  //     output: {
+  //       title,
+  //       summary,
+  //     },
+  //     ...repo,
+  //   });
 };
