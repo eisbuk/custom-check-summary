bff9753baaa6e8657c73c18349a439c64ea32509
ikusteu <ivan.kust57@gmail.com>
2021-12-05 15:56:23 +0100

    Reorganize files, add logging

--- src/createSummary.ts
+++ src/createSummary.ts
@@ -6,62 +6,59 @@ export default async ({
   summaryTitle = "Custom check summary",
   token,
   index,
+  sha,
 }: {
+  sha: string;
   filepath: string;
   token: string;
   summaryTitle?: string;
   index: number;
 }) => {
-  //   const octokit = github.getOctokit(token);
+  const octokit = github.getOctokit(token);
+  const { repo } = github.context;
 
-  console.log("Event name > ", github.context.eventName);
-  console.log("SHA > ", github.context.payload.pull_request.head.sha);
-  //   const {
-  //     repo,
-  //     payload: {
-  //       head_commit: { id: sha },
-  //     },
-  //   } = github.context;
+  console.log(`Creating summary from file: ${filepath}`);
 
-  //   // create title for custom check summary
-  //   const titlePrefix = `${summaryTitle} ${index}:`;
-  //   const fileTitle = filepath
-  //     .split("/")
-  //     .slice(-1)[0]
-  //     .replace(/[-_]/g, " ")
-  //     .replace(/\.(md|html|log)/, "");
-  //   const title = [titlePrefix, `"${fileTitle}"`].join(" ");
+  // create title for custom check summary
+  const titlePrefix = `${summaryTitle} ${index}:`;
+  const fileTitle = filepath
+    .split("/")
+    .slice(-1)[0]
+    .replace(/[-_]/g, " ")
+    .replace(/\.(md|html|log)/, "");
+  const title = [titlePrefix, `"${fileTitle}"`].join(" ");
 
-  //   // create empty (in-progress) summary
-  //   const {
-  //     data: { id },
-  //   } = await octokit.rest.checks.create({
-  //     head_sha: sha,
-  //     name: title,
-  //     status: "in_progress",
-  //     output: {
-  //       summary: "",
-  //     },
-  //     ...repo,
-  //   });
+  // create empty (in-progress) summary
+  const {
+    data: { id },
+  } = await octokit.rest.checks.create({
+    head_sha: sha,
+    name: title,
+    status: "in_progress",
+    output: {
+      title,
+      summary: "",
+    },
+    ...repo,
+  });
 
-  //   // read file to update
-  //   const summary = await new Promise<string>((res) => {
-  //     fs.readFile(filepath, (err, data) => {
-  //       if (err) throw err;
-  //       res(data.toString());
-  //     });
-  //   });
+  // read file to update
+  const summary = await new Promise<string>((res) => {
+    fs.readFile(filepath, (err, data) => {
+      if (err) throw err;
+      res(data.toString());
+    });
+  });
 
-  //   // update check summary
-  //   await octokit.rest.checks.update({
-  //     check_run_id: id,
-  //     status: "completed",
-  //     conclusion: "success",
-  //     output: {
-  //       title,
-  //       summary,
-  //     },
-  //     ...repo,
-  //   });
+  // update check summary
+  await octokit.rest.checks.update({
+    check_run_id: id,
+    status: "completed",
+    conclusion: "success",
+    output: {
+      title,
+      summary,
+    },
+    ...repo,
+  });
 };
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,9 @@
 import * as core from "@actions/core";
+import * as github from "@actions/github";
 import path from "path";
-import fs from "fs";
 
+import { getSha, getFiles } from "./utils";
 import createSummary from "./createSummary";
-
-const getFiles = (dirname: string) =>
-  new Promise<[string[], Error | null]>((res) => {
-    fs.readdir(dirname, (err, files) => {
-      if (err) {
-        return res([[], err]);
-      }
-
-      const filePaths = files.map((fName) => path.join(dirname, fName));
-      return res([filePaths, null]);
-    });
-  });
 
 (async () => {
   // load action inputs
@@ -22,43 +11,21 @@ const getFiles = (dirname: string) =>
   const dirname = core.getInput("FILE_DIR");
   const summaryTitle = core.getInput("SUMMARY_TITLE") as string | undefined;
 
+  // get sha for current event
+  const sha = getSha(github.context);
+
+  // load filepaths of files to create summaries from
   const dirpath = path.join(process.cwd(), dirname);
+  console.log(`Looking for log files in ${dirname}`);
   const [filesToRead, err] = await getFiles(dirpath);
   if (err) throw err;
+  if (!filesToRead.length) {
+    console.log("No summary files found, exiting gracefully");
+    return;
+  }
 
   const createSummaryJobs = filesToRead.map((filepath, index) =>
-    createSummary({ filepath, token, index: index + 1, summaryTitle })
+    createSummary({ filepath, token, index: index + 1, summaryTitle, sha })
   );
   await Promise.all(createSummaryJobs);
 })();
-
-// const getCheckRunContext = (): { sha: string; runId: number } => {
-//   if (github.context.eventName === "workflow_run") {
-//     core.info(
-//       "Action was triggered by workflow_run: using SHA and RUN_ID from triggering workflow"
-//     );
-//     const event = github.context
-//       .payload as EventPayloads.WebhookPayloadWorkflowRun;
-//     if (!event.workflow_run) {
-//       throw new Error(
-//         "Event of type 'workflow_run' is missing 'workflow_run' field"
-//       );
-//     }
-//     return {
-//       sha: event.workflow_run.head_commit.id,
-//       runId: event.workflow_run.id,
-//     };
-//   }
-
-//   const runId = github.context.runId;
-//   if (github.context.payload.pull_request) {
-//     core.info(
-//       `Action was triggered by ${github.context.eventName}: using SHA from head of source branch`
-//     );
-//     const pr = github.context.payload
-//       .pull_request as EventPayloads.WebhookPayloadPullRequestPullRequest;
-//     return { sha: pr.head.sha, runId };
-//   }
-
-//   return { sha: github.context.sha, runId };
-// };
